4126fe754a723f8b56a91617a787f36eb5b3b47e
Mariano Timistit <mtimistit@ius.austral.edu.ar>
2020-07-26 18:02:45 -0300

    correcion

---
+++ The Coffee Store 2/js/main.js
@@ -0,0 +1,12 @@
+var backgroundBlack = document.querySelector("#background2")
+var title = document.querySelector("#title")
+var viewPortHeight = window.innerHeight
+
+window.addEventListener("scroll", parallax)
+
+function parallax () {
+    var scrolled = window.pageYOffset
+    var rate = (1 / viewPortHeight) * scrolled
+    backgroundBlack.style.opacity = rate
+    title.style.transform = "translateY(" + scrolled / 2 + "px)"
+}
